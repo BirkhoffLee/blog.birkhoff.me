a1005817dfdb56d6694cbd60b90c8f1ac2dec9c4
Birkhoff Lee <admin@birkhoff.me>
2019-02-22 20:03:00 +0800

    change telegram notification text format

--- telegram_notify_build.sh
+++ telegram_notify_build.sh
@@ -1,6 +1,12 @@
-curl --data chat_id="$TELEGRAM_CHATID" --data disable_web_page_preview=true --data-binary @- https://api.telegram.org/bot$TELEGRAM_BOT_APIKEY/sendMessage <<EOF
-text=$TRAVIS_REPO_SLUG <$TRAVIS_COMMIT>
-$TRAVIS_COMMIT_MESSAGE
+curl \
+  --data chat_id="$TELEGRAM_CHATID" \
+  --data disable_web_page_preview=true \
+  --data parse_mode=HTML \
+  --data-binary @- https://api.telegram.org/bot$TELEGRAM_BOT_APIKEY/sendMessage <<EOF
+text=
+[TravisCI] <a href="https://travis-ci.org/$TRAVIS_REPO_SLUG">$TRAVIS_REPO_SLUG</a>
+
+<a href="https://github.com/$TRAVIS_REPO_SLUG/commit/$TRAVIS_COMMIT">${TRAVIS_COMMIT:0:7}</a>: $TRAVIS_COMMIT_MESSAGE
 
 The commit has been successfully been deployed to https://blog.birkhoff.me.
 
